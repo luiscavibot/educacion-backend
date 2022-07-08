c305fa878a1583ce42b58734779d49734c246626
Luis Castillo <lcastillov123@gmail.com>
2022-07-08 08:36:34 -0500

    login listo

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { initSwagger } from './app.swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   // global prefix
   initSwagger(app);
   app.setGlobalPrefix('api/v1');
