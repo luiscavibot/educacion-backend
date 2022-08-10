16e13e7762478e2b598dc2a1a28660901a7a39b0
Alejandro Villanueva <alejonc15@email.com>
2022-08-10 16:56:02 -0500

    API asignaturas

--- src/modules/asignaturas/asignaturas.service.ts
+++ src/modules/asignaturas/asignaturas.service.ts
@@ -1,4 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Asignatura } from './entity/asignatura.entity';
+import { Repository } from 'typeorm';
+import { EditAsignaturaDto } from './dtos/edit-asignatura.dto';
+import { CreateAsignaturaDto } from './dtos/create-asignatura.dto';
+import { Observable, from, map } from 'rxjs';
 
 @Injectable()
-export class AsignaturasService {}
+export class AsignaturasService {
+  constructor(
+    @InjectRepository(Asignatura)
+    private readonly asignaturaRepository: Repository<Asignatura>,
+  ) {}
+
+  asignaturaPorCarrera(slug: string): Observable<Asignatura[]> {
+    return from(
+      this.asignaturaRepository.find({
+        order: { created_at: 'ASC' },
+        where: {
+          carrera: {
+            slug,
+          },
+        },
+      }),
+    ).pipe(map((asignaturas: Asignatura[]) => asignaturas));
+  }
+
+  async getById(id: number, asignaturaEntity?: Asignatura) {
+    const carrera = await this.asignaturaRepository
+      .findOne({ where: { id } })
+      .then((d) =>
+        !asignaturaEntity ? d : !!d && asignaturaEntity.id === d.id ? d : null,
+      );
+    if (!carrera)
+      throw new NotFoundException('Asignatura no existe o no está autorizado');
+    return carrera;
+  }
+
+  async createAsignatura(dto: CreateAsignaturaDto) {
+    const asignaturaExiste = await this.asignaturaRepository.findOne({
+      where: { nombre: dto.nombre },
+    });
+    if (asignaturaExiste)
+      throw new BadRequestException('Asignatura ya registrada con ese nombre');
+
+    const nuevaAsignatura = this.asignaturaRepository.create(dto);
+    const asignatura = await this.asignaturaRepository.save(nuevaAsignatura);
+
+    return asignatura;
+  }
+
+  async editAsignatura(
+    id: number,
+    dto: EditAsignaturaDto,
+    asignaturaEntity?: Asignatura,
+  ) {
+    const asignatura = await this.getById(id, asignaturaEntity);
+    const asignaturaEditada = Object.assign(asignatura, dto);
+    return await this.asignaturaRepository.save(asignaturaEditada);
+  }
+
+  async deleteAsignatura(id: number, asignaturaEntity?: Asignatura) {
+    const carrera = await this.getById(id, asignaturaEntity);
+    return await this.asignaturaRepository.remove(carrera);
+  }
+}
--- src/modules/asignaturas/entity/asignatura.entity.ts
+++ src/modules/asignaturas/entity/asignatura.entity.ts
@@ -1,1 +1,45 @@
-export class AsignaturaEntity {}
+import { Carrera } from '../../carreras/entity/carrera.entity';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('asignaturas')
+export class Asignatura extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'text', nullable: false })
+  nombre: string;
+
+  @Column({ type: 'varchar', length: '10' })
+  semestre: string;
+
+  @Column({ type: 'text' })
+  anio: string;
+
+  @Column({ type: 'float' })
+  credito: number;
+
+  @Column({ type: 'boolean' })
+  tipo: boolean;
+
+  @ManyToOne(() => Carrera, (carrera) => carrera.asignaturas)
+  @JoinColumn({ name: 'carreraId' })
+  carrera: Carrera;
+
+  @Column({ type: 'int' })
+  carreraId: number;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+  updated_at: Date;
+}
