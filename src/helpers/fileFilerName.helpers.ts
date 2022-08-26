11b0c87020c5d9d3a342b4529683de6e3763d796
Alejandro Villanueva <alejonc15@email.com>
2022-08-26 14:06:51 -0500

    api eventos

--- src/helpers/fileFilerName.helpers.ts
+++ src/helpers/fileFilerName.helpers.ts
@@ -1,9 +1,11 @@
 export const fileFilterName = (file: any, hash: string) => {
-  const fileName = file.originalname.split('.').replace(' ', '-').toLowerCase();
+  const fileName = file.originalname
+    .split('.')[0]
+    .replace(' ', '-')
+    .toLowerCase();
   const fileExptension = file.mimetype.split('/')[1];
   const validExtensions = ['jpg', 'jpeg', 'png', 'pdf'];
 
-  console.log(fileName);
   if (validExtensions.includes(fileExptension)) {
     return `${fileName}-${hash}.${fileExptension}`;
   }
