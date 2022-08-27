2128a587e88ffc52e19af1705b97d7a4b98564a9
Alejandro Villanueva <alejonc15@email.com>
2022-08-27 15:15:14 -0500

    configuracion para deploy

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   // global prefix
   initSwagger(app);
   // app.setGlobalPrefix('api/v1');
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
