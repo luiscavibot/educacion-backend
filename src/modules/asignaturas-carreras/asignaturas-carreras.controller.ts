2d70cbb659e3f34120b2ca57cf9d199b94890aa0
Alejandro Villanueva <alejonc15@email.com>
2022-09-21 17:11:00 -0500

    update asignaturas

--- src/modules/asignaturas-carreras/asignaturas-carreras.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('asignaturas-carreras')
-export class AsignaturasCarrerasController {}
--- src/modules/asignaturas-carreras/dtos/edit-asignatura-carrera.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAsignaturaCarreraDto } from './create-asignatura-carrera.dto';
-export class EditAsignaturaCarreraDto extends PartialType(
-  CreateAsignaturaCarreraDto,
-) {}
