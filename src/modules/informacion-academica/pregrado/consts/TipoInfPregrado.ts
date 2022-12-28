f6d7ce08b9451d11b466551213bb9031d21bbf6d
Alejandro Villanueva <alejonc15@email.com>
2022-12-28 10:10:51 -0500

    feat: add new type entries for undergraduate academic information

--- src/modules/informacion-academica/pregrado/consts/TipoInfPregrado.ts
+++ src/modules/informacion-academica/pregrado/consts/TipoInfPregrado.ts
@@ -1,6 +1,8 @@
 export enum  TipoInfPregrado {
     'SYLLABUS' = 'Syllabus',
     'HORARIOS' = 'Horarios', 
-    'PLANESESTUDIOS' = 'Planes de estudios', 
+    'PLANESESTUDIOS' = 'Planes de estudios',
+    'PLANESCURRICULARES' ='Planes curriculares', 
+    'MALLASCURRICULARES' ='Mallas curriculares', 
     'OTROS' = 'Otros recursos', 
   }
