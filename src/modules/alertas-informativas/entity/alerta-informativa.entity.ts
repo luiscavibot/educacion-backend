eced6e0eb38c81009c9c1f83fa22fee236cdb0e7
Diego Zavala <diegozavala.code@gmail.com>
2023-02-28 16:34:44 -0500

    feat: add new alerta-informativa entity

---
+++ src/modules/alertas-informativas/entity/alerta-informativa.entity.ts
@@ -0,0 +1,45 @@
+import { User } from "src/modules/users/entities";
+import { 
+    BaseEntity, 
+    Column, 
+    CreateDateColumn, 
+    Entity, 
+    JoinColumn, 
+    ManyToOne, 
+    PrimaryGeneratedColumn, 
+    UpdateDateColumn 
+} from "typeorm";
+
+@Entity('alertas_informativas')
+export class AlertaInformativa extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', nullable: false, length: 50 })
+    titulo!: string;
+
+    @Column({ type: 'varchar', nullable: false, length: 80 })
+    descripcion!: string;
+
+    @Column({ type: 'varchar', nullable: false })
+    url!: string;
+
+    @Column({ type: 'int' })
+    last_updated_by: number;
+
+    @Column({ type: 'boolean' })
+    publicado: boolean;
+
+    @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+    created_at: Date;
+
+    @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+    updated_at: Date;
+
+    @ManyToOne(() => User, (user) => user.alertas_informativas)
+    @JoinColumn({ name: 'usuario_id' })
+    user: User;
+
+    @Column({ type: 'int' })
+    usuario_id: number;
+}
---
+++ src/modules/alertas-informativas/entity/index.ts
@@ -0,0 +1,1 @@
+export * from './alerta-informativa.entity';
