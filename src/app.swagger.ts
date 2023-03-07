6d70dbdf494e23f8cb787c9bd2853115de2c57ba
Diego Zavala <diegozavala.code@gmail.com>
2023-03-07 11:41:16 -0500

    refactor: update swagger configuration

--- src/app.swagger.ts
+++ src/app.swagger.ts
@@ -1,12 +1,15 @@
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { INestApplication } from '@nestjs/common';
+import { json, urlencoded } from 'express';
+import multer from 'multer';
 
 export const initSwagger = (app: INestApplication) => {
   const swaggerConfig = new DocumentBuilder()
-    .setTitle('Educacion API')
+    .setTitle('UNMSM-Services-API')
     .setDescription(
-      'Esta es una API Creada con NestJS para la pagina web de la facultad de educación y está desplegada en AWS',
+      'Esta es una API creada con NestJS para las facultades y dependencias de la UNMSM y está desplegada en AWS',
     )
+    .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('/docs', app, document);
