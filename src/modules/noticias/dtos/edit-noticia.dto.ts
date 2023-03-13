2b276af7c938935062ea7ed692ad7cfa45c33880
Diego Zavala <diegozavala.code@gmail.com>
2023-03-13 10:18:35 -0500

    refactor: remove unnecessary comments

--- src/modules/noticias/dtos/edit-noticia.dto.ts
+++ src/modules/noticias/dtos/edit-noticia.dto.ts
@@ -1,6 +1,5 @@
 import { OmitType } from '@nestjs/swagger';
 import { CreateNoticiaDto } from './create-noticia.dto';
 export class EditNoticiaDto extends OmitType(CreateNoticiaDto, [
-  // 'facultadId',
   'usuario_id',
 ] as const) {}
