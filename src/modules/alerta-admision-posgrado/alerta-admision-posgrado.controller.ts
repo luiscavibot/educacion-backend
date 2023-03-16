737adaf3149bcaedba76f00983b347868e1a5bf6
Diego Zavala <diegozavala.code@gmail.com>
2023-03-16 16:53:18 -0500

    refactor: update alerta-admision-posgrado controller

--- src/modules/alerta-admision-posgrado/alerta-admision-posgrado.controller.ts
+++ src/modules/alerta-admision-posgrado/alerta-admision-posgrado.controller.ts
@@ -7,112 +7,147 @@ import { AlertaAdmisionPosgrado } from './entity';
 @Controller('alertas-admision-posgrado')
 @ApiTags('Alertas Admisión Posgrado')
 export class AlertaAdmisionPosgradoController {
-    constructor(private readonly alertaAdmisionPosgradoService: AlertaAdmisionPosgradoService) {}
+  constructor(private readonly alertaAdmisionPosgradoService: AlertaAdmisionPosgradoService) { }
 
-    @Post()
-    @ApiOperation({
-      description: 'Crea una nueva alerta admisión posgrado',
-    })
-    @ApiResponse({
-      status: 201,
-      description: 'alerta admisión posgrado creada correctamente',
-    })
-    @ApiResponse({
-      status: 409,
-      description: `La alerta admisión posgrado existe`,
-    })
-    @ApiBody({
-      description: 'Crea una nueva alerta admisión posgrado usando una AlertaInformativaDto',
-      type: CreateAlertaAdmisionPosgradoDto,
-      examples: {
-        ejemplo1: {
-          value: {
-            titulo: "Nueva alerta",
-            usuario_id: 0
-          },
+  @Post()
+  @ApiOperation({
+    description: 'Crea una nueva alerta admisión posgrado',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'alerta admisión posgrado creada correctamente',
+  })
+  @ApiResponse({
+    status: 409,
+    description: `La alerta admisión posgrado existe`,
+  })
+  @ApiBody({
+    description: 'Crea una nueva alerta admisión posgrado usando una AlertaInformativaDto',
+    type: CreateAlertaAdmisionPosgradoDto,
+    examples: {
+      ejemplo1: {
+        value: {
+          titulo: "Nueva alerta",
+          usuario_id: 1
         },
       },
-    })
-    async createAlertaAdmisionPosgrado(
-        @Body() dto: CreateAlertaAdmisionPosgradoDto,
-        @Res() response
-    ) {
-        try {
-            const data = await this.alertaAdmisionPosgradoService.createAlertaAdmisionPosgrado({ ...dto });
-            response.status(HttpStatus.CREATED).json({
-              status: HttpStatus.CREATED,
-              message: 'Creación exitosa',
-              data,
-            });
-        } catch (error) {
-            response.status(HttpStatus.FORBIDDEN).json({
-              status: HttpStatus.FORBIDDEN,
-              message: 'Hubo un error al crear registro',
-              error: error.message,
-            });
-        }
+      ejemplo2: {
+        value: {
+          titulo: "Nueva alerta",
+          usuario_id: 1,
+          descripcion: 'Nueva descripción',
+          ciclo: '2023-I'
+        },
+      },
+      ejemplo3: {
+        value: {
+          titulo: "Nueva alerta",
+          usuario_id: 1,
+          descripcion: 'Nueva descripción',
+          ciclo: '2023-I',
+          fecha_inicio: '2023-01-01',
+          fecha_fin: '2023-12-31'
+        },
+      },
+    },
+  })
+  async createAlertaAdmisionPosgrado(
+    @Body() dto: CreateAlertaAdmisionPosgradoDto,
+    @Res() response
+  ) {
+    try {
+      const data = await this.alertaAdmisionPosgradoService.createAlertaAdmisionPosgrado({ ...dto });
+      response.status(HttpStatus.CREATED).json({
+        status: HttpStatus.CREATED,
+        message: 'Creación exitosa',
+        data,
+      });
+    } catch (error) {
+      response.status(HttpStatus.FORBIDDEN).json({
+        status: HttpStatus.FORBIDDEN,
+        message: 'Hubo un error al crear registro',
+        error: error.message,
+      });
     }
+  }
 
-    @Get(':slug/publicada')
-    @ApiOperation({
-      description: 'Devuelve la alerta de admisión posgrado publicada de una facultad',
-    })
-    @ApiParam({
-      name: 'slug',
-      type: String,
-      required: true,
-      description: 'url de la facultad',
-    })
-    async getAlertaAdmisionPosgradoPublicada(
-        @Param('slug') slug: string
-    ): Promise<AlertaAdmisionPosgrado> {
-        const alertaAdmisionPosgrado = await this.alertaAdmisionPosgradoService.getAlertaAdmisionPosgradoPublicada(slug);
-        return alertaAdmisionPosgrado;
-    }
-  
-    @Put(':slug')
-    @ApiOperation({
-      description: 'Actualiza y publica una alerta de admisión posgrado',
-    })
-    @ApiParam({
-      name: 'slug',
-      type: String,
-      required: true,
-      description: 'Slug de la facultad',
-    })
-    @ApiResponse({
-      status: 200,
-      description: 'Se ha actualizado correctamente',
-    })
-    async editAlertaInformativa(
-      @Param('slug') slug: string
-    ) {
-  
-      let data;
-      data = await this.alertaAdmisionPosgradoService.editAlertaAdmisionPosgrado(slug);
-      return { message: 'Alerta de admisión posgrado actualizada y publicada', data };
-    }
-  
-    @Delete(':slug')
-    @ApiOperation({
-      description: 'Borra a una alerta de admisión posgrado de una facultad',
-    })
-    @ApiParam({
-      name: 'slug',
-      type: String,
-      required: true,
-      description: 'url de la facultad',
-    })
-    @ApiResponse({
-      status: 200,
-      description: 'Se ha borrado la alerta de admisión posgrado correctamente',
-    })
-    async deleteAlertaAdmisionPosgrado(
-      @Param('slug') slug: string
-    ) {
-      let data;
-      data = await this.alertaAdmisionPosgradoService.deleteAlertaAdmisionPosgrado(slug);
-      return { message: 'Alerta de admisión posgrado eliminada', data };
-    }
+  @Put(':slug/editar')
+  @ApiOperation({
+    description: 'Actualiza una alerta de admisión posgrado',
+  })
+  @ApiParam({
+    name: 'slug',
+    type: String,
+    required: true,
+    description: 'url de la facultad',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Se ha actualizado correctamente',
+  })
+  @ApiBody({
+    description: 'Actualiza una alerta de admisión posgrado usando una AlertaAdmisionPosgradoDto',
+    type: EditAlertaAdmisionPosgradoDto,
+    examples: {
+      ejemplo1: {
+        value: {
+          titulo: 'Título de alerta editado',
+          descripcion: 'Alerta de admisión posgrado editada',
+          ciclo: '2023-I',
+          publicado: 0,
+          fecha_inicio: '2023-01-01',
+          fecha_fin: '2023-12-31'
+        },
+      }
+    },
+  })
+  async editAlertaAdmisionPosgrado(
+    @Param('slug') slug: string,
+    @Body() dto: EditAlertaAdmisionPosgradoDto,
+  ) {
+
+    let data;
+    data = await this.alertaAdmisionPosgradoService.editAlertaAdmisionPosgrado(slug, dto);
+    return { message: 'Alerta de admisión posgrado actualizada', data };
+  }
+
+  @Get(':slug/vigente')
+  @ApiOperation({
+    description: 'Devuelve la alerta de admisión posgrado vigente de una facultad',
+  })
+  @ApiParam({
+    name: 'slug',
+    type: String,
+    required: true,
+    description: 'url de la facultad',
+  })
+  async getAlertaAdmisionPosgradoVigente(
+    @Param('slug') slug: string
+  ): Promise<AlertaAdmisionPosgrado> {
+    const alertaAdmisionPosgrado = await this.alertaAdmisionPosgradoService.getAlertaAdmisionPosgradoVigente(slug);
+    return alertaAdmisionPosgrado;
+  }
+
+  @Delete(':slug')
+  @ApiOperation({
+    description: 'Borra a una alerta de admisión posgrado de una facultad',
+  })
+  @ApiParam({
+    name: 'slug',
+    type: String,
+    required: true,
+    description: 'url de la facultad',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Se ha borrado la alerta de admisión posgrado correctamente',
+  })
+  async deleteAlertaAdmisionPosgrado(
+    @Param('slug') slug: string
+  ) {
+    let data;
+    data = await this.alertaAdmisionPosgradoService.deleteAlertaAdmisionPosgrado(slug);
+    return { message: 'Alerta de admisión posgrado eliminada', data };
+  }
 
 }
