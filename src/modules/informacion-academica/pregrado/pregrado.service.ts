dc21c256a1cb4f0021b642fa437f7786563f87a1
luiscavibot <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2023-03-19 10:34:15 -0500

    Fix: changue Like patron to find a "escuela" in "Información académica"

--- src/modules/informacion-academica/pregrado/pregrado.service.ts
+++ src/modules/informacion-academica/pregrado/pregrado.service.ts
@@ -10,220 +10,213 @@ import { YearsInfPregrado, TipoInfPregrado } from './consts/';
 
 @Injectable()
 export class PregradoService {
+  constructor(
+    @InjectRepository(Pregrado)
+    private readonly pregradoRepository: Repository<Pregrado>,
+  ) {}
 
-    constructor(
-        @InjectRepository(Pregrado)
-        private readonly pregradoRepository: Repository<Pregrado>,
-    ){}
+  async getById(id: number, pregradoEntity?: Pregrado) {
+    const pregrado = await this.pregradoRepository
+      .findOne({ where: { id } })
+      .then((d) =>
+        !pregradoEntity ? d : !!d && pregradoEntity.id === d.id ? d : null,
+      );
 
-    async getById(id: number, pregradoEntity?: Pregrado){
-        const pregrado = await this.pregradoRepository
-        .findOne({ where : { id }})
-        .then((d) =>
-            !pregradoEntity ? d: !!d && pregradoEntity.id === d.id ? d: null,
-        );
+    return pregrado;
+  }
 
-        return pregrado;
+  paginacionPregrado(
+    options: IPaginationOptions,
+    slug: string,
+    sort: string,
+    estado: string,
+    escuelas: string[],
+    recursos: string[],
+    query: string,
+    fijado: boolean,
+    anio: string,
+  ): Observable<Pagination<Pregrado>> {
+    let order_by = sort?.split(':')[0] || 'id';
+    let direction = sort?.split(':')[1] || 'DESC';
+    let condition: any = [];
+    let _where: FindOptionsWhere<Pregrado>[] = [
+      {
+        user: { facultad: { slug } },
+        fijado,
+        anio,
+      },
+    ];
+    let _select: FindOptionsSelect<Pregrado> = {
+      id: true,
+      nombre: true,
+      estado: true,
+    };
+    if (fijado) {
+      _where = [
+        {
+          user: { facultad: { slug } },
+          fijado,
+        },
+      ];
+    }
+    if (anio) {
+      _where = [
+        {
+          user: { facultad: { slug } },
+          fijado,
+          anio,
+        },
+      ];
+    }
+    if (estado && estado == 'true') {
+      _select = {
+        ..._select,
+        escuela: true,
+        fijado: true,
+        tipo: true,
+        anio: true,
+        updated_at: true,
+        url: true,
+      };
+      _where = [..._where];
     }
 
-    paginacionPregrado(
-        options: IPaginationOptions,
-        slug: string,
-        sort: string,
-        estado: string,
-        escuelas: string[], 
-        recursos: string[],
-        query: string,
-        fijado: boolean,
-        anio: string
-    ): Observable<Pagination<Pregrado>>{
-        let order_by = sort?.split(':')[0] || 'id';
-        let direction = sort?.split(':')[1] || 'DESC';
-        let condition:any = []; 
-        let _where: FindOptionsWhere<Pregrado>[] = [{
-            user: { facultad: { slug } },
-            fijado,
-            anio
-        }];
-        let _select: FindOptionsSelect<Pregrado> = {
-            id: true,
-            nombre: true,
-            estado: true,
+    if (query?.length > 0) {
+      _where = [
+        {
+          user: { facultad: { slug } },
+          fijado,
+          anio,
+          nombre: Like(`%${query}%`),
+        },
+      ];
+    }
+
+    if (escuelas?.length > 0) {
+      escuelas.map((escuela) => {
+        if (recursos?.length > 0) {
+          recursos.map((recurso) => {
+            condition.push({ escuela, recurso });
+          });
+        } else {
+          condition.push({ escuela });
         }
-        if(fijado){
-            _where = [{
-                user: { facultad: { slug } },
-                fijado,
-              }]
-        }
-        if(anio){
-            _where = [{
-                user: { facultad: { slug } },
-                fijado,
-                anio
-            }] 
-        }
-        if(estado && estado == 'true'){
-            _select = {
-                ..._select,
-                escuela: true,
-                fijado: true,
-                tipo: true,
-                anio: true,
-                updated_at:true,
-                url: true
+      });
+    } else {
+      if (recursos?.length > 0) {
+        recursos.map((recurso) => {
+          condition.push({ recurso });
+        });
+      }
+    }
+
+    if (condition?.length > 0) {
+      for (let idx = 0; idx < condition.length; idx++) {
+        if (idx == 0) {
+          if (condition[idx].escuela && condition[idx].recurso) {
+            _where = [
+              {
+                ..._where[0],
+                escuela: Like(`${condition[idx].escuela}`),
+                tipo: Like(`%${condition[idx]?.recurso}%`),
+              },
+            ];
+          } else {
+            if (condition[idx].escuela) {
+              _where = [
+                { ..._where[0], escuela: Like(`${condition[idx].escuela}`) },
+              ];
             }
-            _where = [..._where ];
-        }
-
-        if(query?.length>0){
-            _where = [{
-                user: { facultad: { slug } },
-                fijado,
-                anio,
-                nombre: Like(`%${query}%`)
-              }]
-        }
-
-        if(escuelas?.length>0){
-            escuelas.map( (escuela) =>{
-                if(recursos?.length>0){
-                    recursos.map( (recurso) =>{
-                        condition.push({escuela, recurso});
-                    })
-                }else{
-                    condition.push({escuela})
-                }
-            })
-        }else{
-            if(recursos?.length>0){
-                recursos.map( (recurso) =>{
-                    condition.push({recurso})  
-                })
+            if (condition[idx].recurso) {
+              _where = [
+                { ..._where[0], tipo: Like(`%${condition[idx]?.recurso}%`) },
+              ];
             }
-
+          }
         }
-
-
-        if (condition?.length>0) {
-            for(let idx = 0; idx< condition.length; idx++){
-              if(idx == 0){
-                if(condition[idx].escuela && condition[idx].recurso){
-                    _where = [
-                        {..._where[0],
-                         escuela: Like(`%${condition[idx].escuela}%`),
-                         tipo: Like(`%${condition[idx]?.recurso}%`)
-                        }
-                    ]
-                }else{
-                  if(condition[idx].escuela){
-                    _where = [
-                        {..._where[0],
-                         escuela: Like(`%${condition[idx].escuela}%`),
-                        }
-                    ]
-                  }  
-                  if(condition[idx].recurso){
-                    _where = [
-                        {..._where[0],
-                         tipo: Like(`%${condition[idx]?.recurso}%`)
-                        }
-                    ]
-                  }  
-                }
-              }
-              if(idx>=1){
-                if(condition[idx].escuela && condition[idx].recurso){
-                    _where = [
-                        ..._where,
-                        {..._where[0], 
-                         escuela: Like(`%${condition[idx]?.escuela}%`),
-                         tipo: Like(`%${condition[idx].recurso}%`)
-                        }
-                      ]
-                }else{
-                  if(condition[idx].escuela){
-                    _where = [
-                        ..._where,
-                        {..._where[0], 
-                         escuela: Like(`%${condition[idx]?.escuela}%`),
-                        }
-                      ]
-                  }  
-                  if(condition[idx].recurso){
-                    _where = [
-                        ..._where,
-                        {..._where[0], 
-                         tipo: Like(`%${condition[idx].recurso}%`)
-                        }
-                      ]
-                  }  
-                }
-              }
-      
+        if (idx >= 1) {
+          if (condition[idx].escuela && condition[idx].recurso) {
+            _where = [
+              ..._where,
+              {
+                ..._where[0],
+                escuela: Like(`${condition[idx]?.escuela}`),
+                tipo: Like(`%${condition[idx].recurso}%`),
+              },
+            ];
+          } else {
+            if (condition[idx].escuela) {
+              _where = [
+                ..._where,
+                { ..._where[0], escuela: Like(`${condition[idx]?.escuela}`) },
+              ];
             }
+            if (condition[idx].recurso) {
+              _where = [
+                ..._where,
+                { ..._where[0], tipo: Like(`%${condition[idx].recurso}%`) },
+              ];
+            }
+          }
         }
-
-
-        return from(
-            this.pregradoRepository.findAndCount({
-                skip: Number(options.page) * Number(options.limit) || 0,
-                take: Number(options.limit) || 3,
-                order: { [order_by]: direction },
-                select: _select,
-                where: _where,
-            }),
-        ).pipe(
-            map(([pregrados, totalPregrados]) =>{
-                const pregradosPageable: Pagination<Pregrado> = {
-                    items: pregrados,
-                    meta: {
-                        currentPage: Number(options.page),
-                        itemCount: pregrados.length,
-                        itemsPerPage: Number(options.limit),
-                        totalItems: totalPregrados,
-                        totalPages: Math.ceil(totalPregrados / Number(options.limit)),
-                    }
-                };
-                return pregradosPageable;
-            })
-        )
-
+      }
     }
 
-    async createPregrado(dto: CreatePregradoDto){
-        const nuevoPregrado = this.pregradoRepository.create(dto);
-        const pregrado = await this.pregradoRepository.save(nuevoPregrado);
+    return from(
+      this.pregradoRepository.findAndCount({
+        skip: Number(options.page) * Number(options.limit) || 0,
+        take: Number(options.limit) || 3,
+        order: { [order_by]: direction },
+        select: _select,
+        where: _where,
+      }),
+    ).pipe(
+      map(([pregrados, totalPregrados]) => {
+        const pregradosPageable: Pagination<Pregrado> = {
+          items: pregrados,
+          meta: {
+            currentPage: Number(options.page),
+            itemCount: pregrados.length,
+            itemsPerPage: Number(options.limit),
+            totalItems: totalPregrados,
+            totalPages: Math.ceil(totalPregrados / Number(options.limit)),
+          },
+        };
+        return pregradosPageable;
+      }),
+    );
+  }
 
-        return { pregrado };
-    }
+  async createPregrado(dto: CreatePregradoDto) {
+    const nuevoPregrado = this.pregradoRepository.create(dto);
+    const pregrado = await this.pregradoRepository.save(nuevoPregrado);
 
-    async editPregrado(
-        id: number,
-        dto: EditPregradoDto,
-        pregradoEntity?: Pregrado,
-    ){
-        const pregrado = await this.getById(id, pregradoEntity);
-        const pregradoEditado = Object.assign(pregrado, dto);
-        return await this.pregradoRepository.save(pregradoEditado);
-    }
+    return { pregrado };
+  }
 
+  async editPregrado(
+    id: number,
+    dto: EditPregradoDto,
+    pregradoEntity?: Pregrado,
+  ) {
+    const pregrado = await this.getById(id, pregradoEntity);
+    const pregradoEditado = Object.assign(pregrado, dto);
+    return await this.pregradoRepository.save(pregradoEditado);
+  }
 
-    async deletePregrado(id:number, pregradoEntity?: Pregrado){
-        const pregrado = await this.getById(id, pregradoEntity);
-        return await this.pregradoRepository.remove(pregrado);        
-    }
+  async deletePregrado(id: number, pregradoEntity?: Pregrado) {
+    const pregrado = await this.getById(id, pregradoEntity);
+    return await this.pregradoRepository.remove(pregrado);
+  }
 
-    yearsInfPregrado() {
-        return YearsInfPregrado.map( year => year );
-    }
+  yearsInfPregrado() {
+    return YearsInfPregrado.map((year) => year);
+  }
 
-
-    tipoInfPregrado() {
-        return Object.keys(TipoInfPregrado).map((key) => ({
-          value: key,
-          label: TipoInfPregrado[key],
-        }));
-    }
-
+  tipoInfPregrado() {
+    return Object.keys(TipoInfPregrado).map((key) => ({
+      value: key,
+      label: TipoInfPregrado[key],
+    }));
+  }
 }
